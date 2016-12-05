72553a0d578e06090737d183843e6795a6d5a785
Richard Silveira <richardleecba@gmail.com>
2016-12-05 14:24:50 -0300

    A lot of...

--- app/src/main/java/samples/richardlee/youtubeplayer/MainActivity.java
+++ app/src/main/java/samples/richardlee/youtubeplayer/MainActivity.java
@@ -1,13 +1,40 @@
 package samples.richardlee.youtubeplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPlaySingle = (Button) findViewById(R.id.btnPlaySingle);
+        Button btnStandalone = (Button) findViewById(R.id.btnStandalone);
+
+        btnPlaySingle.setOnClickListener(this);
+        btnStandalone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = null;
+
+        switch (view.getId()) {
+            case R.id.btnPlaySingle:
+                intent = new Intent(this, YoutubeActivity.class);
+                break;
+            case R.id.btnStandalone:
+                intent = new Intent(this, StandAloneActivity.class);
+                break;
+        }
+
+        startActivity(intent);
+
     }
 }
